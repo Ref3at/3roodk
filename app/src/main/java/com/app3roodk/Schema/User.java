d0dd349b6c39838865b02a10b02ec53eae72b004
Hazem-Madkour <hazem.madkor@gmail.com>
2016-05-07 11:00:41 +0200

    Schema-adding rates

--- app/src/main/java/com/app3roodk/Schema/User.java
+++ app/src/main/java/com/app3roodk/Schema/User.java
@@ -1,6 +1,8 @@
 package com.app3roodk.Schema;
 
 
+import java.util.HashMap;
+
 /**
  * Created by ZooM- on 4/23/2016.
  */
@@ -23,12 +25,14 @@ public class User {
     // ref3t add
     private String subsShopsIds;
 
-
+    //rate
+    private HashMap<String,String> commentsRate;
+    private HashMap<String,String> offersRate;
 
     public User() {
     }
 
-    public User(String objectId, String name, String email, String createdAt, String profileImgId, String lat, String lon, String city, String governate, String type, String favOfferIds, String favShopIds, String planId, String subsCategoryIds) {
+    public User(String objectId, String name, String email, String createdAt, String profileImgId, String lat, String lon, String city, String governate, String type, String favOfferIds, String favShopIds, String planId, String subsCategoryIds,HashMap<String,String> commentsRate,HashMap<String,String> offersRate) {
         this.objectId = objectId;
         this.name = name;
         this.email = email;
@@ -43,6 +47,8 @@ public class User {
         this.favShopIds = favShopIds;
         this.planId = planId;
         this.subsCategoryIds = subsCategoryIds;
+        this.commentsRate =commentsRate;
+        this.offersRate = offersRate;
     }
 
     public String getObjectId() {
@@ -163,5 +169,35 @@ public class User {
 
     public void setSubsShopsIds(String subsShopsIds) {
         this.subsShopsIds = subsShopsIds;
+    }
+
+    public HashMap<String, String> getCommentsRate() {
+        return commentsRate;
+    }
+
+    public void setCommentsRate(HashMap<String, String> commentsRate) {
+        this.commentsRate = commentsRate;
+    }
+
+    public void addCommentsRate(HashMap<String, String> commentsRate) {
+        this.commentsRate.putAll(commentsRate);
+    }
+    public void addCommentsRate(String key, String Value) {
+        this.commentsRate.put(key,Value);
+    }
+
+    public HashMap<String, String> getOffersRate() {
+        return offersRate;
+    }
+
+    public void setOffersRate(HashMap<String, String> offersRate) {
+        this.offersRate = offersRate;
+    }
+
+    public void addOffersRate(HashMap<String, String> offersRate) {
+        this.offersRate.putAll(offersRate);
+    }
+    public void addOffersRate(String key, String Value) {
+        this.offersRate.put(key,Value);
     }
 }
