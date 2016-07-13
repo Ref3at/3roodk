2273e674f5bef909b19883c1f2b9652ef661d8af
Hazem Madkour <hazem.madkor@gmail.com>
2016-07-13 06:48:12 +0200

    Add intro Screen fix Login page

--- app/src/main/java/com/app3roodk/UI/OffersCards/CardsActivity.java
+++ app/src/main/java/com/app3roodk/UI/OffersCards/CardsActivity.java
@@ -1,8 +1,12 @@
 package com.app3roodk.UI.OffersCards;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -10,9 +14,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GravityCompat;
@@ -26,25 +32,36 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app3roodk.Constants;
 import com.app3roodk.R;
+import com.app3roodk.Schema.Shop;
+import com.app3roodk.Schema.User;
 import com.app3roodk.UI.About.AboutActivity;
 import com.app3roodk.UI.FavoritesCards.FavoritesActivity;
 import com.app3roodk.UI.Feedback.FeedbackActivity;
 import com.app3roodk.UI.Offer.AddNewOfferActivity;
-import com.app3roodk.UI.PositioningActivity.PositioningActivity;
 import com.app3roodk.UI.Shop.ListShopsActivity;
 import com.app3roodk.UI.Shop.ShopActivity;
+import com.app3roodk.UtilityFirebase;
 import com.app3roodk.UtilityGeneral;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.BitmapImageViewTarget;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class CardsActivity extends AppCompatActivity {
@@ -89,8 +106,7 @@ public class CardsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (!UtilityGeneral.isRegisteredUser(getBaseContext())) {
-                    PositioningActivity.stay = true;
-                    startActivity(new Intent(getBaseContext(), PositioningActivity.class));
+                    signingClick();
                     return;
                 }
                 if (!UtilityGeneral.isShopExist(getBaseContext())) {
@@ -101,6 +117,13 @@ public class CardsActivity extends AppCompatActivity {
             }
         });
         showUserInfoNavigationDrawer();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Constants.RC_SIGN_IN) signingResult(resultCode);
+
     }
 
     public void initNavigationDrawer() {
@@ -130,9 +153,7 @@ public class CardsActivity extends AppCompatActivity {
 
                             case R.id.action_signin:
                                 mDrawerLayout.closeDrawer(GravityCompat.END);
-                                PositioningActivity.stay = true;
-                                startActivity(new Intent(CardsActivity.this, PositioningActivity.class));
-                                finish();
+                               signingClick();
                                 return true;
 
                             case R.id.action_new_shop:
@@ -355,6 +376,155 @@ public class CardsActivity extends AppCompatActivity {
             mDrawerLayout.closeDrawer(GravityCompat.END);
         hideDrawerItems();
     }
+
+    //region Signing
+    User signingUser;
+    ProgressDialog signingProgress;
+
+    private void signingClick() {
+        if (!getLatLng(Constants.PERMISSION_MAPS_SIGN_IN)) return;
+        signingUser = new User();
+        try {
+            LatLng latLng = UtilityGeneral.getCurrentLonAndLat(getBaseContext());
+            signingUser.setLat(String.valueOf(latLng.latitude));
+            signingUser.setLon(String.valueOf(latLng.longitude));
+        } catch (Exception ex) {
+            Toast.makeText(getBaseContext(), "ممكن تفتح الخرائط على الأقل مرة", Toast.LENGTH_LONG).show();
+        }
+        startActivityForResult(
+                UtilityFirebase.getAuthIntent(),
+                Constants.RC_SIGN_IN);
+    }
+
+    private void showSigningProgressDialog() {
+        signingProgress = ProgressDialog.show(this, "تسجيل الدخول",
+                "جاري التحميل...", true);
+    }
+
+    private void setLoadingVisibility(int visibility) {
+        if (visibility == View.VISIBLE) {
+            if (!(signingProgress != null && signingProgress.isShowing()))
+                showSigningProgressDialog();
+        } else if (visibility == View.GONE) {
+            if (signingProgress.isShowing()) signingProgress.dismiss();
+            initNavigationDrawer();
+        }
+    }
+
+    private void signingResult(int resultCode) {
+        try {
+            if (resultCode == RESULT_OK) {
+                setLoadingVisibility(View.VISIBLE);
+                FirebaseUser acct = FirebaseAuth.getInstance().getCurrentUser();
+                signingUser.setObjectId(acct.getUid());
+                signingUser.setEmail(acct.getEmail());
+                signingUser.setName(acct.getDisplayName());
+                signingUser.setNotificationToken(FirebaseInstanceId.getInstance().getToken());
+                UtilityFirebase.getUser(acct.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User temp = dataSnapshot.getValue(User.class);
+                        if (temp == null) {
+                            UtilityFirebase.getUser(signingUser.getObjectId()).setValue(signingUser, new DatabaseReference.CompletionListener() {
+                                @Override
+                                public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                                    if (databaseError != null) {
+                                        Toast.makeText(getBaseContext(), "حصل مشكله شوف النت ", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        UtilityGeneral.saveUser(getBaseContext(), signingUser);
+                                    }
+                                    setLoadingVisibility(View.GONE);
+                                }
+                            });
+                        } else {
+                            signingUser = temp;
+                            UtilityFirebase.getUserShops(signingUser.getObjectId()).addListenerForSingleValueEvent(
+                                    new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            try {
+                                                ArrayList<Shop> shops = new ArrayList<>();
+                                                Iterator<DataSnapshot> iterator = dataSnapshot.getChildren().iterator();
+                                                while (iterator.hasNext())
+                                                    shops.add(iterator.next().getValue(Shop.class));
+                                                UtilityGeneral.saveShops(getBaseContext(), shops);
+                                            } catch (Exception ex) {
+                                            }
+                                            UtilityGeneral.saveUser(getBaseContext(), signingUser);
+                                            setLoadingVisibility(View.GONE);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                            Log.w("Test", "getShop:onCancelled", databaseError.toException());
+                                            setLoadingVisibility(View.GONE);
+                                        }
+                                    });
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            } else {
+                Toast.makeText(getBaseContext(), "فشل فى تسجيل الدخول", Toast.LENGTH_LONG).show();
+                setLoadingVisibility(View.GONE);
+            }
+        } catch (Exception ex) {
+            Log.e("signingResult", ex.getMessage());
+        }
+    }
+
+    private boolean getLatLng(int PERMISSION_TYPE) {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(getBaseContext(), "Open Location First", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+        } else {
+            try {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            PERMISSION_TYPE);
+                } else {
+                    return true;
+                }
+
+            } catch (Exception ex) {
+                Toast.makeText(getBaseContext(), "ممكن تفتح الخرائط على الأقل مرة", Toast.LENGTH_LONG).show();
+            }
+        }
+        return false;
+    }
+
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case Constants.PERMISSION_MAPS_SIGN_IN: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    LatLng latLng = UtilityGeneral.getCurrentLonAndLat(getBaseContext());
+                    signingUser.setLat(String.valueOf(latLng.latitude));
+                    signingUser.setLon(String.valueOf(latLng.longitude));
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setProviders(
+                                            AuthUI.EMAIL_PROVIDER,
+                                            AuthUI.GOOGLE_PROVIDER,
+                                            AuthUI.FACEBOOK_PROVIDER)
+                                    .setTheme(R.style.FirbaseUITheme)
+                                    .build(),
+                            Constants.RC_SIGN_IN);
+                }
+            }
+        }
+    }
+
+    //endregion
 
     static class Adapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
