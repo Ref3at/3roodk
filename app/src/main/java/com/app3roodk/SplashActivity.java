ea19f1944edc2a61d508a2ec736a335bfa7f6174
refaat <muhamedmahmmod@gmail.com>
2016-07-26 19:30:03 +0300

    fix api 17 out of memory error

--- app/src/main/java/com/app3roodk/SplashActivity.java
+++ app/src/main/java/com/app3roodk/SplashActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
+import com.app3roodk.UI.Feedback.FeedbackActivity;
 import com.app3roodk.UI.IntroActivity.IntroActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -17,7 +18,8 @@ public class SplashActivity extends AppCompatActivity {
         handler1.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+               startActivity(new Intent(SplashActivity.this, IntroActivity.class));
+               // startActivity(new Intent(SplashActivity.this, FeedbackActivity.class));
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
             }
