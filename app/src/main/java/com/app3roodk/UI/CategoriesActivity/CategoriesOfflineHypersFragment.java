5a33f8a82dc7504cbd93cb580459baf5497cc27b
Hazem Madkour <hazem.madkor@gmail.com>
2016-09-30 19:22:29 +0200

    fix opening issues 4.4-

--- app/src/main/java/com/app3roodk/UI/CategoriesActivity/CategoriesOfflineHypersFragment.java
+++ app/src/main/java/com/app3roodk/UI/CategoriesActivity/CategoriesOfflineHypersFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -153,7 +152,6 @@ public class CategoriesOfflineHypersFragment extends Fragment {
             }
 
         } catch (Exception ex) {
-            Log.e("erooor", ex.getMessage());
         }
     }
 
--- app/src/main/java/com/app3roodk/UI/CategoriesActivity/CategoriesOfflineShopsFragment.java
+++ app/src/main/java/com/app3roodk/UI/CategoriesActivity/CategoriesOfflineShopsFragment.java
@@ -6,7 +6,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -366,7 +365,7 @@ public class CategoriesOfflineShopsFragment extends Fragment {
             }
 
         } catch (Exception ex) {
-            Log.e("erooor", ex.getMessage().toString());
+
         }
     }
 
